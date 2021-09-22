f377b70de3ae528865db174cec6adfa02deb62be
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-09-22 04:45:34 -0300

    nuevo

--- main.js
+++ main.js
@@ -1,63 +1,51 @@
-let listOfPicture = [
-    {
-       id: 1,
-       Img: "fotos/che4.jpg"
-    },
-    {
-        id: 2,
-        Img: "fotos/che1.jpg"
-     },
-     {
-        id: 3,
-        Img: "fotos/che2.jpg"
-     },
-     {
-        id: 4,
-        Img: "fotos/che3cachafaz.jpg"
-     },
-     {
-        id: 5,
-        Img: "fotos/chesudaka3.jpg",
-     }
-
-
+let listOfPicture =[
+    {img: "fotos/che4.jpg"},
+    {img: "fotos/che1.jpg"},
+    {img: "fotos/che2.jpg"},
+    {img: "fotos/che3cachafaz.jpg"},
+    {img: "fotos/chesudaka3.jpg"}
 ]
 
-let gallerySite = document.getElementById("galleryPrint")
-console.log(gallerySite)
+let galleryPrincipal = document.getElementById("galleryPrincipal")
+let gallerySecundary = document.getElementById("gallerySecundary")
 
 
 
-function printGallery(){
-    gallerySite.innerHTML=
-    `<div class ="gallery__box--principal">
-            <div class ="box gallery__xlBox__img">
-                <img class = "gallery__img" src="${listOfPicture[0].Img}" alt="foto Che sudaka">
-            </div>
-    </div>
-    <div class ="gallery__box--secundary">    
-            <div class ="box gallery__smallBox__img">
-                <img class = "gallery__img uno" src="${listOfPicture[1].Img}" alt="foto Che sudaka">
-            </div>   
-            <div class ="box gallery__smallBox__img">
-                <img class = "gallery__img dos" src="${listOfPicture[2].Img}" alt="foto Che sudaka">
-            </div>
-            <div class ="box gallery__smallBox__img">
-                <img class = "gallery__img tres" src="${listOfPicture[3].Img}" alt="foto Che sudaka">
-            </div>
-            <div class ="box gallery__smallBox__img">
-                <img class = "gallery__img cuatro" src="${listOfPicture[4].Img}" alt="foto Che sudaka">
-            </div>
-    </div>`
+function printPrincipalPhoto(){
+    let principalPhoto = listOfPicture[listOfPicture.length-1];
+    
+    galleryPrincipal.innerHTML = `
+                                    <div class ="box gallery__xlBox__img">
+                                            <img class = "gallery__img" src="${principalPhoto.img}" alt="foto Che sudaka">
+                                    </div>
+                                `
+}
+function printSecundaryPhoto(){
+    let secundaryPhoto = listOfPicture.slice(0,4);
+    console.log(secundaryPhoto)
+    gallerySecundary.innerHTML =`
+                                    <div class ="box gallery__smallBox__img">
+                                        <img class = "gallery__img uno" src="${secundaryPhoto.img}" alt="foto Che sudaka">
+                                    </div>
+                                    <div class ="box gallery__smallBox__img">
+                                        <img class = "gallery__img uno" src="${secundaryPhoto.img}" alt="foto Che sudaka">
+                                    </div>
+                                    <div class ="box gallery__smallBox__img">
+                                        <img class = "gallery__img uno" src="${secundaryPhoto.img}" alt="foto Che sudaka">
+                                    </div>
+                                    <div class ="box gallery__smallBox__img">
+                                        <img class = "gallery__img uno" src="${secundaryPhoto.img}" alt="foto Che sudaka">
+                                    </div>
+                                `
 }
 
 function loadGallery(){
-    
-    listOfPicture.forEach((object, index)=> printGallery(object, index))
-};
+    printPrincipalPhoto();
+    printSecundaryPhoto();
+
+}
 
 loadGallery();
-
 
 
 
