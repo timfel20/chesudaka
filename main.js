7c017441a9177ce97e48764a65875bb8f5b93b3e
LucasManuelRodriguez <rodriguez.m.lucas@gmail.com>
2021-09-22 08:05:30 -0300

    nuevo gallery

--- main.js
+++ main.js
@@ -6,45 +6,39 @@ let listOfPicture =[
     {img: "fotos/che4.jpg"}
 ]
 
-let galleryPrincipal = document.getElementById("galleryPrincipal")
-let gallerySecundary = document.getElementById("gallerySecundary")
+let galleryPrincipal = document.getElementById("galleryPrincipal");
+let gallerySecundary = document.getElementById("gallerySecundary");
 
-
-
-function printPrincipalPhoto(){
-    let principalPhoto = listOfPicture[listOfPicture.length-1];
-    
-    galleryPrincipal.innerHTML = `
-                                    <div class ="box gallery__xlBox__img">
-                                            <img class = "gallery__img" src="${principalPhoto.img}" alt="foto Che sudaka">
-                                    </div>
-                                `
-}
-function printSecundaryPhoto(){
-    listOfPicture.pop();
+function orderPicture(){
     listOfPicture.reverse();
-    for(let i=0; i<listOfPicture.length;i++){
-        let secundaryPhoto = listOfPicture[i]
-        gallerySecundary.innerHTML += `
-                                        <div class ="box gallery__smallBox__img">
-                                             <img class = "gallery__img uno" src="${secundaryPhoto.img}" alt="foto Che sudaka">
-                                        </div>
-                                    `
+}
+
+function printElemento(i,photo){
+    if(i==0){
+        galleryPrincipal.innerHTML =`
+                                    <div class ="box gallery__xlBox__img">
+                                        <img class = "gallery__img" src="${photo}" alt="foto Che sudaka">
+                                    </div>
+                                    ` 
     }
-    
-  
-        
-    
-    
-
+    else{
+        gallerySecundary.innerHTML +=`
+                                    <div class ="box gallery__smallBox__img">
+                                        <img class = "gallery__img uno" src="${photo}" alt="foto Che sudaka">
+                                    </div>
+                                    `
+        }
 }
 
-function loadGallery(){
-    printPrincipalPhoto();
-    printSecundaryPhoto();
-}
+function loadElemento(){
+    orderPicture();
+    for(let i=0;i<listOfPicture.length;i++){
+        let photo = listOfPicture[i].img
+        printElemento(i,photo);
+    }
 
-loadGallery();
+}
+loadElemento();
 
 
 
